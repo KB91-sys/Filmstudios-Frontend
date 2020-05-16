7be46764c76ac9e44757c724b30b65145bf2a54f
Kristofer <kristofer_b_1991@hotmail.com>
2020-05-16 20:26:28 +0200

    Tillägg av login/logut funktion

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,9 @@
 
-getFilmList();
+GetFilmList();
 
 
 //Funktionen för att ta emot alla filmer
-function getFilmList(){
+function GetFilmList(){
     fetch("https://localhost:44361/api/film")
         .then(function (fetchedData){
             return fetchedData.json();        
@@ -56,20 +56,18 @@ function getFilmList(){
 //Tar emot alla filmstudios
 fetch("https://localhost:44361/api/filmstudio")
     .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(errorCode => console.log("ERROR"));
+    .then(data => console.log(data));
+    
 
     //Tar emot alla filmstrivia
 fetch("https://localhost:44361/api/filmTrivia")
     .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(errorCode => console.log("ERROR"));
+    .then(data => console.log(data));
 
     //Tar emot alla lånade filmer
 fetch("https://localhost:44361/api/rentedFilm")
     .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(errorCode => console.log("ERROR"));
+    .then(data => console.log(data));
 
 
 
@@ -77,32 +75,151 @@ fetch("https://localhost:44361/api/rentedFilm")
 
 
 
-// LOGIN KOD
+    
+    
 
-// NOTIS: InnerHTML ersätter allt med nytt material. Kan vara bra att använda när man vill ta bort en vy
-// insertadjacenthtml = dela upp och lägga till nytt innehåll i en vy
+    
+    
+    // NOTIS: InnerHTML ersätter allt med nytt material. Kan vara bra att använda när man vill ta bort en vy
+    // insertadjacenthtml = dela upp och lägga till nytt innehåll i en vy
+    
+    var header = document.getElementById("headerContent");
+    var navigation = document.getElementById("navigation");
 
-var logedInPage = document.getElementById("logedinContent");
+
+
+    // Kollar om användaren är inloggad genom att söka genom localstorage
+    if(localStorage.getItem("userId") !== null)
+    {
+
+        var name = localStorage.getItem("userId");
+
+        LoginPage(name);
+    }
+    else{
+                
+        StartPage();
+        
+    }
+    
+    
+    
+    //localStorage.clear();
+    
+    // STARTSIDAN
+    function StartPage(){
+        
+        header.innerHTML = "";
+        
+        // HEADER //
+        header.insertAdjacentHTML("afterbegin", '<h1 id="heading0">SVENSKA FILMSTUDIOS </h1><h3 id="heading1">uthyrningstjänst</h3><input id="usernameInputbox" type="text" placeholder="Username...">    <input id="passwordInputbox" type="password" placeholder="Password..."><button id="loginButton">Login</button>');
+        
+        // NAVIGATION //
+        navigation.innerHTML = "";
+        navigation.innerHTML = ('<h3 id="registerUser">Registrera ny användare</h3>');
+        
+        //Input text och knapp
+        var loginButton = document.getElementById("loginButton");
+                
+        loginButton.addEventListener("click", function () { 
+            
+            
+            var passwordInput = document.getElementById("passwordInputbox").value;
+            var usernameInput = document.getElementById("usernameInputbox").value;
+            
+            console.log("Du har tryckt på login knappen!");
+            console.log("Username: " + usernameInput + " Password: " + passwordInput);
+            
+            
+            
+            fetch("users.json")
+            .then(function(data){
+                
+                return data.json();
+            
+            
+            })
+            .then(function(jsonData){
+
+                console.log(jsonData);
+
+                for(i = 0; i < jsonData.length; i++){
+
+                    if(usernameInput == jsonData[i].userName && passwordInput == jsonData[i].password){
+
+
+                        localStorage.setItem("userId", usernameInput);
+                        
+                        LoginPage(usernameInput);
+                        
+
+                    }
+
+
+
+                }
 
 
 
 
-var loginButton = document.getElementById("loginButton");
-
-loginButton.addEventListener("click", function() { 
+            });
+                       
+            
+        });
         
         
-    var passwordInput = document.getElementById("passwordInputbox").value;
-    var usernameInput = document.getElementById("usernameInputbox").value;
+        
+        
+    }
     
-    console.log("Du har tryckt på login knappen!")
-    console.log("Username: " + usernameInput + " Password: " + passwordInput);
+        
+    // INLOGGAD ANVÄNDARES VY
+    function LoginPage(usernameInput){
+        
+        header.innerHTML = " ";
+
+
+        // HEADER //
+        header.insertAdjacentHTML("afterbegin", '<h1 id="heading0">SVENSKA FILMSTUDIOS </h1><h3 id="heading1">uthyrningstjänst</h3><button id="logoutButton">Logout</button>');
+        
+        var createh2 = document.createElement('h2');
+        createh2.appendChild(document.createTextNode("Välkommen " + usernameInput));
+        header.appendChild(createh2);        
+        
+        // NAVIGATION //
+        navigation.innerHTML = "";
+        navigation.innerHTML = ('<h3>Låna film</h3><h3>Lämna tillbaka film</h3><h3>Skriv en trivia</h3>');
+        
+        
+        var logout = document.getElementById("logoutButton");
+
+        // LOGUT KNAPP //        
+        logout.addEventListener("click", function () {
+            console.log("Du tryckte på logout knappen. Går tillbaka till startPage"); 
+            
+            localStorage.clear();
+            
+            StartPage();
+        
+        
+        
+        });
+        
+        
+        
+        
+    }
     
     
-} );
     
+
     
+    //Skapar ny användare
     
-    
-    
+    function createNewUser(){
+        
+        
+        
+        
+    }
     
